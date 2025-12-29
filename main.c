b599e6973a5e5ad2ed51e7e459e6948b9d0835b8
vybukhivka <viktorbakhovka@gmail.com>
2025-12-29 19:25:23 +0200

    user input prompted in a loop

--- main.c
+++ main.c
@@ -18,24 +18,28 @@ void editTask(void) {
 int main(void) {
 	char input[30];
 	char allowedInputs[4][10] = {"list", "add", "complite", "edit"};
+	int isInputAllowed = 0;
 
-	printf("input: ");
-	fgets(input, sizeof(input), stdin);
+	do {
+		printf("input: ");
+		fgets(input, sizeof(input), stdin);
+		size_t length = strlen(input);
+		input[length - 1] = '\0';
 
-	size_t length = strlen(input);
-	input[length - 1] = '\0';
+		for (int i = 0; i < 4; i++) {
+			int comparisonResult = strcmp(input, allowedInputs[i]);
+			if (comparisonResult == 0) {
+				isInputAllowed = 1;
+				break;
+			}
+		}
 
-	for (int i = 0; i < 4; i++) {
-		int comparisonResult = strcmp(input, allowedInputs[i]);
-
-		if (comparisonResult == 0) {
-			printf("you input is %s\n", allowedInputs[i]);
-			break;
+		if (isInputAllowed == 1) {
+			printf("You good to go\n");
 		} else {
 			printf("You input in not allowed\n");
-			return 1;
 		}
-	}
+	} while (isInputAllowed == 0);
 
 	return 0;
 }
