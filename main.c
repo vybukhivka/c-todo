93b849cb99e4af69cc728bd7e31074583a86423c
vybukhivka <viktorbakhovka@gmail.com>
2025-12-30 19:22:15 +0200

    main function formatting

--- main.c
+++ main.c
@@ -56,37 +56,35 @@ void appendToFile(char *task) {
 }
 
 int main(void) {
-    char input[30];
-    char allowedInputs[4][10] = {"list", "add", "complite", "edit"};
-    int isInputAllowed = 0;
+	char input[30];
+	char allowedInputs[4][10] = {"list", "add", "complite", "edit"};
+	int isInputAllowed = 0;
 
-    createDataFile();
+	createDataFile();
 
-    do {
-	printf("input: ");
-	fgets(input, sizeof(input), stdin);
-	size_t length = strlen(input);
-	input[length - 1] = '\0';
+	do {
+		printf("input: ");
+		fgets(input, sizeof(input), stdin);
+		size_t length = strlen(input);
+		input[length - 1] = '\0';
 
-	for (int i = 0; i < 4; i++) {
-	    int comparisonResult = strcmp(input, allowedInputs[i]);
-	    if (comparisonResult == 0) {
-		isInputAllowed = 1;
-		break;
-	    }
+		for (int i = 0; i < 4; i++) {
+			int comparisonResult = strcmp(input, allowedInputs[i]);
+			if (comparisonResult == 0) {
+				isInputAllowed = 1;
+				break;
+			}
+		}
+
+		if (isInputAllowed == 0) {
+			printf("You input in not allowed\n");
+		}
+	} while (isInputAllowed == 0);
+
+	if (strcmp(input, "list\n")) {
+		showTasks();
 	}
 
-	if (isInputAllowed == 1) {
-	    printf("You good to go\n");
-	} else {
-	    printf("You input in not allowed\n");
-	}
-    } while (isInputAllowed == 0);
-
-    if (strcmp(input, "list\n")) {
-	showTasks();
-    }
-
-    return 0;
+	return 0;
 }
 
